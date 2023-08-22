2ceb9ebb1807bcbdacd558d4a28e8ca84aa4ec08
jose17s <130622077+jose17s@users.noreply.github.com>
2023-08-22 13:59:04 -0500

    Taller Terminado

---
+++ src/Taller/NumerosDeDosEnDos.java
@@ -0,0 +1,9 @@
+package Taller;
+
+public class NumerosDeDosEnDos {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 200; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
